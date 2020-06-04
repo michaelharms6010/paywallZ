f53367ebc42bbebdd7dc2f3743ab2decdd3dcc3b
Michael Harms <michael.harms6010@gmail.com>
2020-06-04 13:18:41 -0500

    adds initial migration

--- data/migrations/20200604131235_ips-sessions-addresses.js
+++ data/migrations/20200604131235_ips-sessions-addresses.js
@@ -1,8 +1,28 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("ips", tbl => {
+      tbl.increments();
+      tbl.string("ip");
+  })
+  .createTable("zaddrs", tbl => {
+      tbl.increments();
+      tbl.string("zaddr").unique().notNullable();
+      tbl.boolean("active").defaultTo(false);
+  })
+  .createTable("sessions", tbl => {
+      tbl.increments();
+      tbl.string("ip")
+      .references("ip")
+      .inTable("ips")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+      tbl.string("content-id");
+      tbl.boolean("paid").defaultTo(false);
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("sessions")
+  .dropTableIfExists("zaddrs")
+  .dropTableIfExists("ips")
 };
