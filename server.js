6963f4d52ece58a679ae2a4d660a72b62b43590f
Michael Harms <michael.harms6010@gmail.com>
2020-06-04 13:10:13 -0500

    removes users

--- server.js
+++ server.js
@@ -3,18 +3,12 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
-const userRouter = require("./users/users-router")
-const authRouter = require("./auth/auth-router")
-
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use(morgan("dev"));
-
-server.use("/auth", authRouter)
-server.use("/users", userRouter)
 
 server.get("/", (req,res) => {
     res.json({message: "Server is up and running"})
