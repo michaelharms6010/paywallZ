ac0afc688c6a104744a5f8091f276ca1d460af87
Michael Harms <michael.harms6010@gmail.com>
2020-06-04 13:27:04 -0500

    adds catch

--- sessions/sessions-router.js
+++ sessions/sessions-router.js
@@ -3,6 +3,12 @@ const Sessions = require("./sessions-model")
 router.get("/", (req, res) => {
     Sessions.getSessions().then(sessions => {
         sessionses.status(200).json(sessions)
-    })
+    }).catch(err => res.status(500))
+})
 
+router.post("/", (req,res) => {
+    const newSession = req.body;
+    Sessions.add(newSession).then(newsession => {
+        res.status(201).json(newSession)
+    }).catch(err => res.status(500))
 })
